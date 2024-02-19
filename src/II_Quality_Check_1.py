7096cab669702fd276cd4414d2dfa22c7adc7f41
KarinaCarlson <156943189+KarinaCarlson@users.noreply.github.com>
2024-02-19 11:53:47 -0500

    Add files via upload

---
+++ src/II_Quality_Check_1.py
@@ -0,0 +1,10 @@
+
+# Quality Check
+    # takes RNA transcript as input
+    # runs FastQC
+# asks user if they want to see MultiQC data
+    # prints out results for user
+    # explains what good results should look like
+        # do we act on results in any way?
+        # do we suggest troubleshooting or links to follow for bad results?
+
