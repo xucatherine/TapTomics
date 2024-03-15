aa99e4b9c55c10b108051a63c6460da4da4439ac
KarinaCarlson <156943189+KarinaCarlson@users.noreply.github.com>
2024-03-15 17:21:12 -0400

    Add files via upload

--- src/A_Set-Up.py
+++ src/A_Set-Up.py
@@ -2,3 +2,9 @@
 ## The things too complicated to download we can just ask the user to deal with through links in README
 ## I called it A_[] because it comes first and data input is now done in the main file
 
+
+# Install requirements:
+
+# MAIN : gitpython, biopython and the SRA Toolkit
+# Quality_Check : fastqc and multiqc
+# Trimming : cutadapt
