43ae73b9f69abfb23474fda921a0fd31b3e5710a
KarinaCarlson <156943189+KarinaCarlson@users.noreply.github.com>
2024-03-15 17:07:13 -0400

    Add files via upload

---
+++ src/A_Set-Up.py
@@ -0,0 +1,4 @@
+# This script downloads and installs whatever we need, e.g. FastQC
+## The things too complicated to download we can just ask the user to deal with through links in README
+## I called it A_[] because it comes first and data input is now done in the main file
+
